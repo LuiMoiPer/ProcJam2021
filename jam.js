15f1d1874fc1bcb39ed3fdb243a317722e811843
LuiMoiPer <luimoiper@outlook.com>
2021-11-22 20:13:29 -0800

    Add start and stop buttons.

--- jam.js
+++ jam.js
@@ -1,9 +1,25 @@
-audioContext = new AudioContext;
+const audioContext = new AudioContext;
 
 gainNode = audioContext.createGain();
 gainNode.connect(audioContext.destination); 
 gainNode.gain.value = 0.1 ** 2;
 
-oscillator = audioContext.createOscillator();
-oscillator.connect(gainNode);
-oscillator.start();
+const startButton = document.createElement("button");
+startButton.innerText = "Start";
+startButton.addEventListener("click", () => {
+    oscillator = audioContext.createOscillator();
+    oscillator.connect(gainNode);
+    oscillator.start();
+});
+document.body.appendChild(startButton);
+
+const stopButton = document.createElement("button");
+stopButton.innerText = "Stop";
+stopButton.addEventListener("click", () => {
+    oscillatorGain = audioContext.createGain();
+    oscillatorGain.gain.value = 1;
+    oscillatorGain.gain.exponentialRampToValueAtTime(0.001, audioContext.currentTime + 0.5);
+    oscillator.connect(oscillatorGain).connect(gainNode);
+    oscillator.stop();
+});
+document.body.appendChild(stopButton);
